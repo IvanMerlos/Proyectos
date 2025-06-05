3b08e733b9f47fbf24ef0f1474d4a150720fe54b
Ivan Mateo Merlos <merlosivan24@gmail.com>
2025-06-05 20:07:40 -0300

    Create ejercicio 5,45 sin terminar.c

---
+++ ejercicio 5,45 sin terminar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int filas,columnas;
+int main(){
+       printf("Ingrese c/de Columnas: ");
+      scanf("%d",&columnas);
+      printf("Ingrese c/de Filas: ");
+      scanf("%d",&filas);
+      
+    for(int i=1;i<=columnas;i++){
+      if(i%columnas == 0){
+       printf("%d\n",i);}
+       for(i){
+        
+       }
+    else
+    {
+     printf("%d",i);
+
+    }
+     
+    }
+      
+       
+    return 0;
+}
