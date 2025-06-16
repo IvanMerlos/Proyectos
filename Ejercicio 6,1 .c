450a02cc4b38c53f7635dafcdc3f7f615cf75512
Ivan Mateo Merlos <merlosivan24@gmail.com>
2025-06-16 11:35:09 -0300

    Create Ejercicio 6,1 .c

---
+++ Ejercicio 6,1 .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+ int Bienvenidos()
+ {
+ printf("Bienvenidos! Comenzando en ");
+ }
+ 
+ int main(){
+  
+  for(int i=10;i>=0;i--){
+   Bienvenidos();
+   printf("%d \n",i);
+  }
+   
+
+ return 0;
