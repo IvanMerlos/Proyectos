20898a10f5ce59b2e36140e46e76920a17f00a59
Ivan Mateo Merlos <merlosivan24@gmail.com>
2025-07-10 13:43:11 -0300

    Update Ejercicio 5,47.c

--- Ejercicio 5,47.c
+++ Ejercicio 5,47.c
@@ -15,3 +15,25 @@ for(int i=1;i<=10;i++){
     printf("El mayor es %d \n",mayor);
     return 0;
 }
+//////////////////////////////////////////////////////////////////////////////otra forma mas extensa de hacerlo
+#include <stdio.h>
+int num1;
+int max=0;
+int main(){
+ 
+for(int i=0;i<=10;i++){
+ printf("\nIngrese un numero: ");
+ scanf("%d",&num1);
+ 
+ if(num1>max){
+  max=num1;
+ }
+ else{
+  
+ max=max; 
+ }
+
+}
+printf("el mayor es %d",max);
+    return 0;
+}
