77a4fe13d99c3e9b94668a0e9cab8e00f671e6d4
Ivan Mateo Merlos <merlosivan24@gmail.com>
2025-08-15 12:11:38 -0300

    Update Ejercicio 7,1.c

--- Ejercicio 7,1.c
+++ Ejercicio 7,1.c
@@ -1,17 +1,22 @@
 //Arreglos 
 #include <stdio.h>
- #define N 3
+ #define N 5
  int main(void)
  {
- int i;
- int a[N];
- 
+  int i;
+  int a[N]={0};
+
  for(i=0;i<N;i++){
-  printf("Ingrese[%d]: ",i);
-  scanf("%d",&a[i]);
-  printf("a[%d]:%d \n",i,a[i]);
+ printf("Ingrese el valor del arreglo a[%d]:",i);
+ scanf("%d",&a[i]);
+ 
+ if(a[i]<0){
+  a[i]=a[i] *-1;
  }
  
- return 0;
+}for(i=0;i<N;i++){
+printf("a[%d]:%d\n",i,a[i]);
+}
+return 0;
  }
  
